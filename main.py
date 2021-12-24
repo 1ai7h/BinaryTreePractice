5ec5905151a1805961572847b3872f20b3e24175
1ai7h <81769715+laithkamal1@users.noreply.github.com>
2021-12-24 12:42:48 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+#Practicing Binary Trees
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def TreePrinter(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.PreOrder(tree.root, "")
+        else:
+            print(str(traversal_type) + " Not Supported, try all lowercase or different traversal type")
+            return False
+
+    def PreOrder(self, start, traversal):
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.PreOrder(start.left, traversal)
+            traversal = self.PreOrder(start.right, traversal)
+        return traversal
+
+
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.right.right = Node(5)
+
+print(tree.TreePrinter("preorder"))
